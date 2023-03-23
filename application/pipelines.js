c88f03826bfa7810c7b014fa58f2f09ce728fd55
skillbot101 <shresh.khatri@gmail.com>
2023-03-23 15:41:37 +0000

    Error removed

--- application/pipelines.js
+++ application/pipelines.js
@@ -1,12 +1,8 @@
 const { removeStopwords } = require('stopword')
-<<<<<<< HEAD
 const customStopwordsQandA=['user','usr','asks','ask','gets','take','for','to','you','he','they','i','is','are','they','the']
 const customStopwordsScrits=['user','usr','asks','ask','gets','take','for','to','you','he','they','i','script','story']
 
 function piplineModelTrainingLog(useremail, projectName,){
-=======
-function pipelineQandAdataWithStaticResponse(email, projectName, locale) {
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
   return [
     {
       '$match': {
@@ -24,7 +20,6 @@ function pipelineQandAdataWithStaticResponse(email, projectName, locale) {
       }
     }, {
       '$project': {
-<<<<<<< HEAD
         'log': '$trainingLog.log'
       }
     }, {
@@ -33,41 +28,12 @@ function pipelineQandAdataWithStaticResponse(email, projectName, locale) {
       }
     }, {
       '$limit': 100
-=======
-        'datasets.intents': 1
-      }
-    }, {
-      '$unwind': {
-        'path': '$datasets.intents'
-      }
-    }, {
-      '$match': {
-        'datasets.intents.linkedScript': { '$exists': false },
-        'datasets.intents.actionName': { '$exists': false }
-      }
-    }
-    , {
-      '$project': {
-        'intent': '$datasets.intents.intent',
-        'description': "$datasets.intents.description",
-        'utterances': '$datasets.intents.utterances',
-        'answers': '$datasets.intents.answers',
-        'actionName': '$datasets.intents.actionName',
-        'actionCode': '$datasets.intents.actionCode'
-      }
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
     }
   ]
 }
 
-<<<<<<< HEAD
 
 function pipelineQandAdataWithStaticResponse(useremail, projectName, locale) {
-=======
-//retreiving list of intents having 'linkedScript' property
-//this is for script section
-function pipelineIntentData(email, projectName, locale) {
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
   return [
     {
       '$match': {
@@ -97,13 +63,7 @@ function pipelineIntentData(email, projectName, locale) {
       }
     }, {
       '$match': {
-<<<<<<< HEAD
         'datasets.intents.linkedScript': { '$exists': false }
-=======
-        'datasets.intents.linkedScript': {
-          '$exists': true
-        }
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
       }
     }
     , {
@@ -111,19 +71,14 @@ function pipelineIntentData(email, projectName, locale) {
         'intent': '$datasets.intents.intent',
         'description': "$datasets.intents.description",
         'utterances': '$datasets.intents.utterances',
-<<<<<<< HEAD
         'answers': '$datasets.intents.answers',
         'actionName': '$datasets.intents.actionName',
         'actionCode': '$datasets.intents.actionCode'
-=======
-        'linkedScript': '$datasets.intents.linkedScript'
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
       }
     }
   ]
 }
 
-<<<<<<< HEAD
 //retreiving list of intents having 'linkedScript' property
 //this is for script section
 function pipelineIntentData(useremail, projectName, locale) {
@@ -179,97 +134,6 @@ function pipelineIntentName(useremail, projectName, locale) {
     {
       '$match': {
         'useremail': useremail,
-=======
-//pipeline for retrieving intents which are not linked to any script yet
-<<<<<<< HEAD
-<<<<<<< HEAD
-//this will be used as list of intents for triggering the stript
-function pipelineIntentName(email, projectName, locale) {
-=======
-=======
->>>>>>> 1c9fe975d7e9bb11bf4b8811799f5261789540f1
-function pipelineIntentName(email, projectName, locale) {
-  return [
-    {
-      '$match': {
-        'email': email,
-        'projectName': projectName
-      }
-    }, {
-      '$project': {
-        '_id': 0,
-        'datasets': 1
-      }
-    }, {
-      '$unwind': {
-        'path': '$datasets'
-      }
-    }, {
-      '$match': {
-        'datasets.locale': locale
-      }
-    }, {
-      '$project': {
-        'datasets.intents': 1
-      }
-    }, {
-      '$unwind': {
-        'path': '$datasets.intents'
-      }
-    }, {
-      '$match': {
-        'datasets.intents.linkedScript': {
-          '$exists': false
-        }
-      }
-    }, {
-      '$project': {
-        'intent': '$datasets.intents.intent'
-      }
-    }
-  ]
-  
-}
-
-
-function pipelineQAndADataForTrainingModel(email,projectName,locale){
-return [
-  {
-    '$match': {
-      'email': email, 
-      'projectName': projectName
-    }
-  }, {
-    '$unwind': {
-      'path': '$datasets'
-    }
-  }, {
-    '$match': {
-      'datasets.locale': locale
-    }
-  }, {
-    '$project': {
-      '_id': 0, 
-      'languages': 1, 
-      'settings': 1, 
-      'locale': '$datasets.locale', 
-      'qanda': '$datasets.qanda', 
-      'entities': '$datasets.entities',
-      'intents': '$datasets.intents',
-      'scripts': '$datasets.scripts'
-    }
-  }
-]
-}
-
-//function to get the list of details for every entities
-function pipelinetListOfEntities(email,projectName,locale){
->>>>>>> 1c9fe975d7e9bb11bf4b8811799f5261789540f1
-  return [
-    {
-      '$match': {
-        'email': email,
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
         'projectName': projectName
       }
     }, {
@@ -307,19 +171,11 @@ function pipelinetListOfEntities(email,projectName,locale){
 }
 
 
-<<<<<<< HEAD
 function pipelineTrainingDataAndSettings(useremail, projectName, locale) {
   return [
     {
       '$match': {
         'useremail': useremail,
-=======
-function pipelineQAndADataForTrainingModel(email, projectName, locale) {
-  return [
-    {
-      '$match': {
-        'email': email,
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
         'projectName': projectName
       }
     }, {
@@ -335,10 +191,7 @@ function pipelineQAndADataForTrainingModel(email, projectName, locale) {
         '_id': 0,
         'languages': 1,
         'settings': 1,
-<<<<<<< HEAD
         'projectFolderName':1,
-=======
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
         'locale': '$datasets.locale',
         'entities': '$datasets.entities',
         'intents': '$datasets.intents',
@@ -351,19 +204,11 @@ function pipelineQAndADataForTrainingModel(email, projectName, locale) {
 }
 
 //function to get the list of details for every entities
-<<<<<<< HEAD
 function pipelinetListOfEntities(useremail, projectName, locale) {
   return [
     {
       '$match': {
         'useremail': useremail,
-=======
-function pipelinetListOfEntities(email, projectName, locale) {
-  return [
-    {
-      '$match': {
-        'email': email,
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
         'projectName': projectName
       }
     }, {
@@ -390,19 +235,11 @@ function pipelinetListOfEntities(email, projectName, locale) {
   ]
 }
 
-<<<<<<< HEAD
 function pipelinetListOfEntityNames(useremail, projectName, locale) {
   return [
     {
       '$match': {
         'useremail': useremail,
-=======
-function pipelinetListOfEntityNames(email, projectName, locale) {
-  return [
-    {
-      '$match': {
-        'email': email,
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
         'projectName': projectName
       }
     }, {
@@ -426,50 +263,16 @@ function pipelinetListOfEntityNames(email, projectName, locale) {
   ]
 }
 
-<<<<<<< HEAD
 function piplineListOfScriptNames(useremail, projectName, locale) {
   return [
     {
       '$match': {
         'useremail': useremail,
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-function piplineListOfScriptNames(email, projectName, locale) {
-  return [
-    {
-      '$match': {
-        'email': email,
-=======
-=======
->>>>>>> 1c9fe975d7e9bb11bf4b8811799f5261789540f1
-function piplineListOfScriptNames(email,projectName,locale){
-  return [
-    {
-      '$match': {
-        'email': email, 
-<<<<<<< HEAD
->>>>>>> 1c9fe975d7e9bb11bf4b8811799f5261789540f1
-=======
->>>>>>> 1c9fe975d7e9bb11bf4b8811799f5261789540f1
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
         'projectName': projectName
       }
     }, {
       '$project': {
-<<<<<<< HEAD
         '_id': 0,
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-        '_id': 0,
-=======
-        '_id': 0, 
->>>>>>> 1c9fe975d7e9bb11bf4b8811799f5261789540f1
-=======
-        '_id': 0, 
->>>>>>> 1c9fe975d7e9bb11bf4b8811799f5261789540f1
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
         'datasets': 1
       }
     }, {
@@ -490,42 +293,19 @@ function piplineListOfScriptNames(email,projectName,locale){
       }
     }, {
       '$project': {
-<<<<<<< HEAD
         'scriptName': '$datasets.scripts.scriptName',
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-        'scriptName': '$datasets.scripts.scriptName',
-=======
-        'scriptName': '$datasets.scripts.scriptName', 
->>>>>>> 1c9fe975d7e9bb11bf4b8811799f5261789540f1
-=======
-        'scriptName': '$datasets.scripts.scriptName', 
->>>>>>> 1c9fe975d7e9bb11bf4b8811799f5261789540f1
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
         'scripDescription': '$datasets.scripts.scriptDescription'
       }
     }
   ]
 }
 
-<<<<<<< HEAD
 //pipeline for retrieving list of fully defined (i.e. intent with either static response or dynamic response)
 function piplineListOfIntentToLinkNames(useremail, projectName, locale) {
   return [
     {
       '$match': {
         'useremail': useremail,
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-//pipeline for retrieving list of fully defined (i.e. intent with either static response or dynamic response)
-function piplineListOfIntentToLinkNames(email, projectName, locale) {
-  return [
-    {
-      '$match': {
-        'email': email,
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
         'projectName': projectName
       }
     }, {
@@ -564,15 +344,9 @@ function piplineListOfIntentToLinkNames(email, projectName, locale) {
   ]
 }
 
-<<<<<<< HEAD
 function queryFindActionName(useremail, projectName, locale, actionName) {
   return {
     'useremail': useremail,
-=======
-function queryFindActionName(email, projectName, locale, actionName) {
-  return {
-    'email': email,
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
     'projectName': projectName,
     'datasets.locale': locale,
     'datasets.actions.actionName': actionName
@@ -580,19 +354,11 @@ function queryFindActionName(email, projectName, locale, actionName) {
 }
 
 
-<<<<<<< HEAD
 function pipelineQandAdataWithDynamicResponse(useremail, projectName, locale) {
   return [
     {
       '$match': {
         'useremail': useremail,
-=======
-function pipelineQandAdataWithDynamicResponse(email, projectName, locale) {
-  return [
-    {
-      '$match': {
-        'email': email,
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
         'projectName': projectName
       }
     }, {
@@ -651,19 +417,11 @@ function pipelineQandAdataWithDynamicResponse(email, projectName, locale) {
   ]
 }
 
-<<<<<<< HEAD
 function pipelineScriptData(useremail, projectName, locale) {
   return [
     {
       '$match': {
         'useremail': useremail,
-=======
-function pipelineScriptData(email, projectName, locale) {
-  return [
-    {
-      '$match': {
-        'email': email,
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
         'projectName': projectName
       }
     }, {
@@ -699,19 +457,11 @@ function pipelineScriptData(email, projectName, locale) {
   ]
 }
 
-<<<<<<< HEAD
 function pipelineActionCode(useremail, projectName, locale, actionName) {
   return [
     {
       '$match': {
         'useremail': useremail,
-=======
-function pipelineActionCode(email, projectName, locale, actionName) {
-  return [
-    {
-      '$match': {
-        'email': email,
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
         'projectName': projectName
       }
     }, {
@@ -748,19 +498,11 @@ function pipelineActionCode(email, projectName, locale, actionName) {
 }
 
 
-<<<<<<< HEAD
 function getQndAdataCount(useremail, projectName, locale) {
   return [
     {
       '$match': {
         'useremail': useremail,
-=======
-function getQndAdataCount(email, projectName, locale) {
-  return [
-    {
-      '$match': {
-        'email': email,
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
         'projectName': projectName
       }
     }, {
@@ -797,7 +539,6 @@ function getQndAdataCount(email, projectName, locale) {
 }
 
 
-<<<<<<< HEAD
 //pipeline for getting script count
 function getScriptsCount(useremail, projectName, locale) {
   return [
@@ -842,15 +583,6 @@ function searchQandAData(useremail, projectName, locale, searchText) {
     {
       '$match': {
         'useremail': useremail,
-=======
-function searchQandAData(email, projectName, locale, searchText) {
-  var tokens = removeStopwords(searchText.split(' '))
-  tokens.push(searchText)
-  return [
-    {
-      '$match': {
-        'email': email,
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
         'projectName': projectName
       }
     }, {
@@ -877,11 +609,7 @@ function searchQandAData(email, projectName, locale, searchText) {
     }, {
       '$match': {
         'intents.linkedScript': {
-<<<<<<< HEAD
              '$exists': false
-=======
-          '$exists': false
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
         }
       }
     }, {
@@ -889,29 +617,20 @@ function searchQandAData(email, projectName, locale, searchText) {
         'intent': '$intents.intent',
         'description': '$intents.description',
         'utterances': '$intents.utterances',
-<<<<<<< HEAD
         'answers': '$intents.answers',
         'actionName': '$intents.actionName',
         'actionCode': '$intents.actionCode'
-=======
-        'answers': '$intents.answers'
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
       }
     }, {
       '$match': {
         '$or': [
           {
             'intent': {
-<<<<<<< HEAD
               '$in': tokens
-=======
-              '$in': tokens.map(token => new RegExp(token))
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
             }
           },
           {
             'description': {
-<<<<<<< HEAD
               '$in': tokens
             }
           }
@@ -971,9 +690,6 @@ function searchScript(useremail, projectName, locale, searchText) {
           }, {
             'description': {
               '$in': tokens
-=======
-              '$in': tokens.map(token => new RegExp(token))
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
             }
           }
         ]
@@ -984,26 +700,11 @@ function searchScript(useremail, projectName, locale, searchText) {
 
 module.exports = {
   pipelineQandAdataWithStaticResponse,
-<<<<<<< HEAD
   pipelineTrainingDataAndSettings,
-=======
-=======
-=======
->>>>>>> 1c9fe975d7e9bb11bf4b8811799f5261789540f1
-module.exports = { 
-  pipelineQandAdata,
->>>>>>> 1c9fe975d7e9bb11bf4b8811799f5261789540f1
-  pipelineQAndADataForTrainingModel,
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
   pipelinetListOfEntities,
   pipelinetListOfEntityNames,
   pipelineIntentData,
   pipelineIntentName,
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
   piplineListOfScriptNames,
   piplineListOfIntentToLinkNames,
   queryFindActionName,
@@ -1011,18 +712,8 @@ module.exports = {
   pipelineScriptData,
   pipelineActionCode,
   getQndAdataCount,
-<<<<<<< HEAD
   getScriptsCount,
   searchQandAData,
   searchScript,
   piplineModelTrainingLog
-=======
-  searchQandAData
-=======
-  piplineListOfScriptNames
->>>>>>> 1c9fe975d7e9bb11bf4b8811799f5261789540f1
-=======
-  piplineListOfScriptNames
->>>>>>> 1c9fe975d7e9bb11bf4b8811799f5261789540f1
->>>>>>> b60442086acbd022b27141726f7da23642ec8ec4
 };
